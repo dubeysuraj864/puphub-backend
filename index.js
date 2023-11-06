eabc8dc3ee98582a53d997f4ae4c409ed8d1f0c2
Suraj Dubey <dubeysuraj864@gmail.com>
2023-11-06 12:24:05 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// import express and morgan
+const express = require("express");
+const morgan = require("morgan");
+require("./db/config")
+const users = require("./db/users")
+// Bring in environmental variables
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+// ALL YOUR MIDDLEWARE AND ROUTES GO HERE
+app.use(cors())
+app.use(morgan("tiny")); // middleware for logging
+app.use(express.urlencoded({ extended: true })); //middleware for parsing urlencoded data
+app.use(express.json()); // middleware for parsing incoming json
+app.use("/static", express.static("static")); // to set a folder for static file serving
+
+app.get("/", (req, res) => {
+  res.send("Hello from node");
+});
+
+app.post("/register", async (req, res)=> {
+    let user = await new  users(req.body)
+    let result = await user.save()
+    res.send(result);
+})
+
+app.post("/login", async (req, res)=> {
+  console.log(req.body)
+  if(req.body.email && req.body.password){
+    let user = await users.findOne(req.body).select("-password")
+    if(user){
+      res.send({user })
+    }
+    else{
+      res.send({result: "No user found"})
+    }
+  }
+})
+
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
